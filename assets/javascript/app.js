55755ca6ed4db6bdac4c470414eb6893082555d4
newmanje11 <jevonte_n@hotmail.com>
2018-12-10 07:59:19 -0500

    add firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyApZDfXVQPLujt2yPP2JeLHlV-6JEHkqcs",
+    authDomain: "train-activity-661b6.firebaseapp.com",
+    databaseURL: "https://train-activity-661b6.firebaseio.com",
+    projectId: "train-activity-661b6",
+    storageBucket: "train-activity-661b6.appspot.com",
+    messagingSenderId: "427734489298"
+  };
+  firebase.initializeApp(config);
